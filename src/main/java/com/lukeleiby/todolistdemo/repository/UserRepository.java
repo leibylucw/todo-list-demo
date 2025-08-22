ee553024ff2e68fbc3c8b5a7e67e9341f4e1193d
Luke Leiby <lucaswleiby@gmail.com>
2025-08-22 01:28:31 -0400

    Remove stale `src` at root directory

--- src/main/java/com/lukeleiby/todolistdemo/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lukeleiby.todolistdemo.repository;
-
-import com.lukeleiby.todolistdemo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
